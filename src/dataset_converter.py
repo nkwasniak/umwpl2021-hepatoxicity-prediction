01e782af3839ef097a27b1db3493fcdf85e52bac
nkwasniak <48225976+nkwasniak@users.noreply.github.com>
2021-12-19 16:12:06 +0100

    Add files via upload

--- src/dataset_converter.py
+++ src/dataset_converter.py
@@ -3,7 +3,7 @@ import unittest
 
 
 def convert_fingerprint_CSV(file_name, first_col):
-    df_values = pd.read_csv("data/" + file_name + ".csv", header=1)
+    df_values = pd.read_csv("../data/" + file_name + ".csv", header=1)
 
     main_column = df_values.iloc[:, 0]
 
@@ -11,13 +11,14 @@ def convert_fingerprint_CSV(file_name, first_col):
     df_after_drop = df_values.drop(df_values.columns[0], axis=1).dropna().reset_index().drop('index', axis=1)
 
     df_after_drop.insert(0, first_col, main_column_wo_nan)
-    df_after_drop.to_csv("data/" + file_name + "_after_conversion.csv", index=False)
+    df_after_drop.to_csv("../data/" + file_name + "_after_conversion.csv", index=False)
+
+    return df_after_drop
 
 
 class TestConverter(unittest.TestCase):
     def setUp(self):
-        convert_fingerprint_CSV('hepatotoxicity_ALT_KlekFP_ready_set', 'ALT')
-        self.data = pd.read_csv("data/hepatotoxicity_pTD50_KlekFP_ready_set_after_conversion.csv", header=0)
+        self.data = convert_fingerprint_CSV('hepatotoxicity_ALT_KlekFP_ready_set', 'ALT')
 
     def test_any_null(self):
         self.assertEqual(False, self.data.isnull().values.any())
@@ -28,5 +29,3 @@ class TestConverter(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-    # convert_fingerprint_CSV('hepatotoxicity_pTD50_KlekFP_ready_set', 'TD50')
-    # convert_fingerprint_CSV('hepatotoxicity_ALT_KlekFP_ready_set', 'ALT')
---
+++ src/dataset_preprocessing.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pandas as pd
+import unittest
+
+
+def preprocessing_data(file_name, first_col):
+    df = pd.read_csv("../data/" + file_name + ".csv", header=0, index_col=False)
+
+    columns = list(df.columns.values)
+    columns.remove('ALT')
+
+    sum_col = df[columns].sum(axis=0)
+    zero_col = list(dict((k, v) for (k, v) in sum_col.items() if v == 0.0).keys())
+
+    df.drop(columns=zero_col, inplace=True)
+
+    df[first_col] = np.log(df[first_col])
+
+    df.to_csv("../data/hepatotoxicity_" + first_col + "_preprocessing_and_analyst.csv", index=False)
+
+    return df
+
+
+class TestConverter(unittest.TestCase):
+    def setUp(self):
+        self.file_name = "hepatotoxicity_ALT_KlekFP_ready_set_after_conversion"
+        self.data = preprocessing_data(self.file_name, 'ALT')
+
+    def test_any_zero_column(self):
+        columns = list(self.data.columns.values)
+        columns.remove('ALT')
+        self.assertEqual(True, self.data[columns].sum(axis=0).min() > 0)
+
+    def test_any_zero_row(self):
+        columns = list(self.data.columns.values)
+        columns.remove('ALT')
+        self.assertEqual(True, self.data[columns].sum(axis=1).min() > 0)
+
+    def test_first_column_natural_log(self):
+        first_column = np.log(pd.read_csv("../data/" + self.file_name + ".csv", header=0, index_col=False, usecols=[0]))
+        self.assertEqual(True, self.data['ALT'].equals(first_column['ALT']))
+
+
+if __name__ == "__main__":
+    unittest.main()
